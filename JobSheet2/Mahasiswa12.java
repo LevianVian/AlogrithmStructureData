40a9c0919dcec855b6cf4e7a4fb21196d080ad1c
LevianVian <artalevi4@gmail.com>
2026-02-19 09:36:23 +0700

    Practicum1

---
+++ JobSheet2/Mahasiswa12.java
@@ -0,0 +1,35 @@
+package JobSheet2;
+
+public class Mahasiswa12 {
+    String nama;
+    String nim;
+    String kelas;
+    Double ipk;
+
+    void tampilkanInformasi(){
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("IPK: " + ipk);
+        System.out.println("Kelas: " + kelas);
+    }
+
+    void ubahKelas(String kelasBaru){
+        kelas = kelasBaru;
+    }
+
+    void udpdateIPK(Double ipkBaru){
+        ipk = ipkBaru;
+    }
+
+    String nilaiKinerja(){
+        if (ipk >= 3.5) {
+            return "Kinerja Sangat Baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja Baik";
+        } else if (ipk >= 2.0) {
+            return "Kinerja Cukup";
+        } else {
+            return "Kinerja Kurang";
+        }
+    }
+}

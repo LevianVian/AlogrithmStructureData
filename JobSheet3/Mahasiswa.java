17da3c51b3539aa17b65a285006009edccb6b07d
LevianVian <artalevi4@gmail.com>
2026-03-01 10:48:27 +0700

    creating printInfo method

--- JobSheet3/Mahasiswa.java
+++ JobSheet3/Mahasiswa.java
@@ -5,4 +5,13 @@ public class Mahasiswa {
     public String name;
     public String grade;
     public float gpa;
+
+    public void printInfo() {
+        System.out.println("NIM        : " + nim);
+        System.out.println("Name       : " + name);
+        System.out.println("Grade      : " + grade);
+        System.out.println("GPA        : " + gpa);
+        System.out.println("-----------------------------------");
+    }
+    
 }

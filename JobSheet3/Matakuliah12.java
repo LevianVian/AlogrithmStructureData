01a6524fba27496cd775fd051c43bd41ad174f62
LevianVian <artalevi4@gmail.com>
2026-03-01 11:11:35 +0700

    Finishing the 3.4.1 Experiment or Matakuliah practicum

---
+++ JobSheet3/Matakuliah12.java
@@ -0,0 +1,18 @@
+package JobSheet3;
+
+public class Matakuliah12 {
+    public String code;
+    public String name;
+    public int sks;
+    public int jumlahJam;
+
+    public Matakuliah12(String code, String name, int sks, int jumlahJam){
+        this.code = code;
+        this.name = name;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+
+    public Matakuliah12() {}
+    
+}

008ecf8658dd60c4c9dd92ad590561d1394c7545
LevianVian <artalevi4@gmail.com>
2026-04-09 17:51:03 +0700

    Add MergeSort implementation and SortMain12 for testing merge sort functionality

---
+++ JobSheet7/week7/SortMain12.java
@@ -0,0 +1,17 @@
+package JobSheet7.week7;
+
+public class SortMain12 {
+    public static void main(String[] args) {
+        int data[] = {10, 40, 30, 50, 70, 20, 100, 90};
+        System.out.println("Sorting with merge sort");
+        MergeSort mSort = new MergeSort();
+
+        System.out.println("Initial Data");
+        mSort.printArray(data);
+
+        mSort.mergeSort(data);
+
+        System.out.println("Sorted Data");
+        mSort.printArray(data);
+    }
+}

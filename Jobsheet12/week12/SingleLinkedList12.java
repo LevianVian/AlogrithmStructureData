2760e1289ee10038032da03a02551273788c6141
LevianVian <artalevi4@gmail.com>
2026-05-07 13:45:57 +0700

    Movin the main, before it was in SingleLinkedList12 now its in SLLMain12

--- Jobsheet12/week12/SingleLinkedList12.java
+++ Jobsheet12/week12/SingleLinkedList12.java
@@ -75,21 +75,4 @@ public class SingleLinkedList12 {
             }
         }
     }
-
-    public static void main(String[] args) {
-        SingleLinkedList12 sll = new SingleLinkedList12();
-        Student12 std1 = new Student12("001", "Student 1", "TI-1I", 3.89);
-        Student12 std2 = new Student12("002", "Student 2", "TI-1I", 3.45);
-        Student12 std3 = new Student12("003", "Student 3", "TI-1I", 3.20);
-        Student12 std4 = new Student12("004", "Student 4", "TI-1I", 3.00);
-
-        sll.print();
-        sll.addFirst(std4);
-        sll.print();
-        sll.addLast(std1);
-        sll.print();
-        sll.insertAfter(std3, "Student 4");
-        sll.insertAt(2, std2);
-        sll.print();
-    }
 }

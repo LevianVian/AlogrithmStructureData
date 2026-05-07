33712d6f14a6523f125b16d397ab1c63e0bc238a
LevianVian <artalevi4@gmail.com>
2026-05-07 13:05:10 +0700

    Experiment 1 Step 5

---
+++ Jobsheet12/week12/Node12.java
@@ -0,0 +1,10 @@
+package Jobsheet12.week12;
+
+public class Node12 {
+    Student12 data;
+    Node12 next;
+    public Node12(Student12 data) {
+        this.data = data;
+        this.next = null;
+    }
+}

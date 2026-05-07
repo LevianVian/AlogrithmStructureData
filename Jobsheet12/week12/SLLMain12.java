8e51de74386ee6377ef58b9cdc446163d0ed973e
LevianVian <artalevi4@gmail.com>
2026-05-07 14:09:37 +0700

    Experiment 2 Done

--- Jobsheet12/week12/SLLMain12.java
+++ Jobsheet12/week12/SLLMain12.java
@@ -5,40 +5,33 @@ import java.util.Scanner;
 public class SLLMain12 {
         public static void main(String[] args) {
         SingleLinkedList12 sll = new SingleLinkedList12();
-        Scanner sc = new Scanner(System.in);
-
         Student12 std1 = new Student12("001", "Student 1", "TI-1I", 3.89);
         Student12 std2 = new Student12("002", "Student 2", "TI-1I", 3.45);
         Student12 std3 = new Student12("003", "Student 3", "TI-1I", 3.20);
         Student12 std4 = new Student12("004", "Student 4", "TI-1I", 3.00);
 
+        sll.print();
         sll.addFirst(std4);
+        sll.print();
         sll.addLast(std1);
+        sll.print();
         sll.insertAfter(std3, "Student 4");
         sll.insertAt(2, std2);
-
-        System.out.println("--- Data Before Keyboard Input (Default) ---");
         sll.print();
 
-        System.out.println("\n--- Add New Student ---");
-        System.out.print("Input NIM: ");
-        String nim = sc.nextLine();
-        
-        System.out.print("Input Name: ");
-        String name = sc.nextLine();
-        
-        System.out.print("Input Class: ");
-        String className = sc.nextLine();
-        
-        System.out.print("Input GPA: ");
-        double gpa = sc.nextDouble();
+        // Experiment 2
+        System.out.println("Data at index 1 is:");
+        Student12 data = sll.getData(1);
+        if (data != null) data.print();
 
-        Student12 newStd = new Student12(nim, name, className, gpa);
-        
-        sll.addLast(newStd); 
+        int idx = sll.indexOf("Student 1");
+        System.out.println("Student 1 is located at index: " + idx);
 
-        System.out.println("\n--- Data After Input ---");
+        sll.removeFirst();
+        sll.removeLast();
         sll.print();
-        sc.close();
+
+        sll.removeAt(0);
+        sll.print();
     }
 }
--- Jobsheet12/week12/SingleLinkedList12.java
+++ Jobsheet12/week12/SingleLinkedList12.java
@@ -75,4 +75,102 @@ public class SingleLinkedList12 {
             }
         }
     }
+
+    // Experiment 2
+    // Step 2 Get data with specific index
+    Student12 getData(int idx) {
+        if(isEmpty()) {
+            System.out.println("LinkedList is empty!!");
+            return null;
+        }
+        Node12 tmp = head;
+        for(int i = 0; i < idx; i++){
+            tmp = tmp.next;
+        }
+        return tmp.data;
+    }
+
+    // Step 3 Get the index of a specific student by name
+    int indexOf(String key) {
+        if(isEmpty()){
+            System.out.println("LinkedList is empty!!");
+            return -1;
+        }
+        Node12 tmp = head;
+        int idx = 0;
+        while(tmp != null && !tmp.data.name.equalsIgnoreCase(key)){
+            tmp = tmp.next;
+            idx++;
+        }
+        if(tmp == null) {
+            return -1;
+        } else {
+            return idx;
+        }
+    }
+
+    // Step 4 Remove the first element
+    void removeFirst(){
+        if(isEmpty()){
+            System.out.println("LinkedList is empty!!");
+        } else if(head == tail){
+            head = tail = null;
+        } else {
+            head = head.next;
+        }
+    }
+
+    // Step 5 Remove the last element
+    void removeLast(){
+        if(isEmpty()){
+            System.out.println("LinkedList is empty!!");
+        } else if(head == tail){
+            head = tail = null;
+        } else {
+            Node12 tmp = head;
+            while(tmp.next != tail){
+                tmp = tmp.next;
+            }
+            tmp.next = null;
+            tail = tmp;
+        }
+    }
+
+    // Step 6 Remove a specific element by name
+    public void remove(String key) {
+        if (isEmpty()) {
+            System.out.println("LinkedList is empty!!");
+        } else {
+            Node12 temp = head;
+            while (temp != null) {
+                if ((temp.data.name.equalsIgnoreCase(key)) && (temp == head)) {
+                    removeFirst();
+                    break;
+                } else if (temp.next != null && temp.next.data.name.equalsIgnoreCase(key)) { // Added temp.next != null for safety
+                    temp.next = temp.next.next;
+                    if (temp.next == null) {
+                        tail = temp;
+                    }
+                    break;
+                }
+                temp = temp.next;
+            }
+        }
+    }
+
+    // Step 7 Remove an element at a specific index
+    public void removeAt(int index) {
+        if (index == 0) {
+            removeFirst();
+        } else {
+            Node12 temp = head;
+            for (int i = 0; i < index - 1; i++) {
+                temp = temp.next;
+            }
+            temp.next = temp.next.next;
+            if (temp.next == null) {
+                tail = temp;
+            }
+        }
+    }
 }
